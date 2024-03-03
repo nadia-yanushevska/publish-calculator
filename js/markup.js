c4abf7884820bdf149062118c74812679e35dbd7
Nadia <nadiazlagoda@gmail.com>
2024-03-04 00:26:22 +0200

    translations

--- js/markup.js
+++ js/markup.js
@@ -2,6 +2,45 @@ export const HIDDEN_CLASS = 'hidden-elements';
 
 const OBJ_TYPE = 'object';
 
+const translations = {
+    format: {
+        displayName: 'Формат',
+        suffix: '',
+    },
+    pages: {
+        displayName: 'Текст',
+        suffix: 'стор.',
+    },
+    symbols: {
+        displayName: 'Обсяг',
+        suffix: 'тис. зн.',
+    },
+    a: {
+        displayName: 'Ширина ілюстр.',
+        suffix: 'см',
+    },
+    b: {
+        displayName: 'Висота ілюстр.',
+        suffix: 'см',
+    },
+    quantity: {
+        displayName: 'Кількість  ілюстр.',
+        suffix: '',
+    },
+    ov: {
+        displayName: 'Обл.-вид.',
+        suffix: '',
+    },
+    yda: {
+        displayName: 'Ум. д. а.',
+        suffix: '',
+    },
+    A4toA5: {
+        displayName: 'Друк А4 на А5',
+        suffix: '',
+    },
+};
+
 export const fieldSetMarkup = `<fieldset class="column-container" data-js-added>
                         <label class="form-lables" for="a">a <input class="form-inputs list-input" name="a" id="a" type="number" disabled  value="10.5" step=".5"  data-a-input /></label>
                         <label class="form-lables" for="b">b <input class="form-inputs list-input" name="b" id="b" type="number" min="0" step=".5" /></label>
@@ -14,6 +53,7 @@ export function getDataMarkup(obj) {
 }
 
 function dataTemplate(propArr) {
-    const valueStr = typeof propArr[1] === OBJ_TYPE ? propArr[1].join(', ') : propArr[1];
-    return `<li class="data-li">${propArr[0]}: <strong>${valueStr}</strong></li>`;
+    let valueStr = typeof propArr[1] === OBJ_TYPE ? propArr[1].join(', ') : typeof propArr[1] === 'boolean' ? 'да' : propArr[1];
+
+    return `<li class="data-li">${translations[propArr[0]].displayName}: <strong>${valueStr}</strong> ${translations[propArr[0]].suffix}</li>`;
 }
---
+++ js/properties.js
@@ -0,0 +1,47 @@
+const translations = {
+    format: {
+        displayName: 'Формат',
+        suffix: '',
+    },
+    pages: {
+        displayName: 'Текст',
+        suffix: 'стор.',
+    },
+    symbols: {
+        displayName: 'Обсяг',
+        suffix: 'тис. зн.',
+    },
+    a: {
+        displayName: 'Ширина ілюстр.',
+        suffix: 'см',
+    },
+    b: {
+        displayName: 'Висота ілюстр.',
+        suffix: 'см',
+    },
+    quantity: {
+        displayName: 'Кількість  ілюстр.',
+        suffix: '',
+    },
+    ov: {
+        displayName: 'Обл.-вид.',
+        suffix: '',
+    },
+    yda: {
+        displayName: 'Ум. д. а.',
+        suffix: '',
+    },
+};
+class Property {
+    propName;
+    displayName;
+    nameSuffix;
+
+    constructor(propName, displayName, nameSuffix) {
+        this.propName = propName;
+        this.displayName = displayName;
+        this.nameSuffix = nameSuffix;
+    }
+}
+
+const properties = [new Property('format', 'Формат', '')];
