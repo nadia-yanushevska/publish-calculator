c6f6842aeab9a77dce52ce55c72575b2061d417a
Nadia <nadiazlagoda@gmail.com>
2024-03-04 00:31:36 +0200

    translations-updated

--- js/markup.js
+++ js/markup.js
@@ -42,9 +42,15 @@ const translations = {
 };
 
 export const fieldSetMarkup = `<fieldset class="column-container" data-js-added>
-                        <label class="form-lables" for="a">a <input class="form-inputs list-input" name="a" id="a" type="number" disabled  value="10.5" step=".5"  data-a-input /></label>
-                        <label class="form-lables" for="b">b <input class="form-inputs list-input" name="b" id="b" type="number" min="0" step=".5" /></label>
-                        <label class="form-lables" for="quantity">Quantity <input class="form-inputs list-input" name="quantity" id="quantity" type="number" min="0" /></label>
+                        <label class="form-lables" for="a"
+                            >Ширина шпальти (см): <input class="form-inputs list-input" name="a" id="a" type="number" disabled value="10.5" step=".5" data-a-input
+                        /></label>
+                        <label class="form-lables" for="b"
+                            >Висота ілюстрації (см): <input class="form-inputs list-input" name="b" id="b" type="number" min="0" step=".5" data-b-input
+                        /></label>
+                        <label class="form-lables" for="quantity"
+                            >Кількість зображень: <input class="form-inputs list-input" name="quantity" id="quantity" type="number" min="0" data-quantity-input
+                        /></label>
                     </fieldset>`;
 
 export function getDataMarkup(obj) {
